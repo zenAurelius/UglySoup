c04687414fb696ca723ed70d9fc1cc78dbdf9ca7
zenAurelius <loic2845@gmail.com>
2021-03-11 17:09:13 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='uglysoup',
+version='0.1',
+description='bad scrapper',
+url='#',
+author='zenAurelius',
+license='MIT',
+packages=['uglysoup'],
+zip_safe=False)
---
+++ uglysoup/__init__.py
@@ -0,0 +1,1 @@
+from .uglysoup import UglySoup
