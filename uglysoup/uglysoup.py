462c0f0bc4f31cc4c0f32db9b4ca30fdf2124754
zenAurelius <loic2845@gmail.com>
2021-04-29 16:45:45 +0200

    Add files via upload

--- uglysoup/uglysoup.py
+++ uglysoup/uglysoup.py
@@ -5,38 +5,38 @@ import html
 
 class UglySoup:
 
-    def __init__(self, s="", a={}, e=0):
+    def __init__(self, s="", a={}, e=0, tag_name=None):
         self.s = str(s)
         self.attr = a
         self.end = e
+        self.tag = tag_name
 
     def find(self, req_deb, req_fin, start=0):
         found = self.s.find(req_deb, start)
         if found < 0:
             return None
 
-        # trouver le tag
+        # find the tag
         tag_start = self.s.rfind("<", 0, found+1)
         m = re.search(" |>", self.s[tag_start:]).start()
-        # tag_name = self.s[tag_start+1: tag_start+m]
+        tag_name = self.s[tag_start+1: tag_start+m]
         tag_end = self.s.find(">", tag_start+1)
         full_tag = self.s[tag_start+1: tag_end]
-        # print(full_tag)
+                
         # trouver les attributs
         a = {}
         att_m = re.finditer('([^\s]+=)(\".*?\")', full_tag)
         for m in att_m:
             a[m.group(1).strip('=')] = html.unescape(m.group(2)).strip('"')
 
-        # trouver le content
+        # find the content - if ending is not found, we go to the end
         end = self.s.find(req_fin, tag_end)
-        # print(self.s[tag_end:tag_end+10000])
-        # print(end)
-        # print(self.s[tag_end+1:end])
+        if end < 0:
+            end = len(self.s)
         content = self.s[tag_end+1:end].strip()
 
-        # retourn une soupe correspondant � la recherche
-        return UglySoup(content, a, end)
+        # return a new soup
+        return UglySoup(content, a, end, tag_name)
 
     def findAll(self, req_deb, req_fin):
         found = []
